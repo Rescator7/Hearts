e5a4403b38faa7934a5208ad9cfacd6b37b4a15b
Rescator7 <36249900+Rescator7@users.noreply.github.com>
2018-11-16 21:24:29 -0500

    Add files via upload
    additional file

---
+++ variants.h
@@ -0,0 +1,9 @@
+#ifndef VARIANTS
+#define VARIANTS
+
+const int perfect_100_value = 50;      // the hand score will be set to this number.
+const int no_trick_value    = 5;       // the no trick bonus will substract this number to the hand score.
+const int omnibus_value     = 10;      // the bonus for winning the jack of diamond.
+const int game_over_score   = 100;     // reach this score to make the game over.
+
+#endif // VARIANTS
